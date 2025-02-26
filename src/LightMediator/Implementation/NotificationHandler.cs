b0fd45d4ee7beca6928f9ae870074c7a79ee532d
Mahmood Malekloo <mahmoodmalekloo@outlook.com>
2025-02-26 16:11:57 +0330

    fix: change the notification mapper to map all notification types

--- src/LightMediator/Implementation/NotificationHandler.cs
+++ src/LightMediator/Implementation/NotificationHandler.cs
@@ -14,38 +14,9 @@ public abstract class NotificationHandler<TNotification> : INotificationHandler 
     public Task HandleNotification(object message, LightMediatorOptions mediatorOptions, CancellationToken? cancellationToken = null)
     {
 
-        var json = JsonConvert.SerializeObject(message);
-        var sourceFields = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-        var targetFields = Activator.CreateInstance(typeof(TNotification));
-        var targetProperties = typeof(TNotification).GetProperties();
-
-        if (!mediatorOptions.IgnoreNotificationDifferences &&
-            (sourceFields == null ||
-            sourceFields.Count() == 0))
-        {
-            throw new NullReferenceException($"Source notification has not property");
-        }
-        if (sourceFields == null)
-            throw new ArgumentNullException();
-
-        if (!mediatorOptions.IgnoreNotificationDifferences && 
-            sourceFields.Any(s => !targetProperties.Any(t => t.Name == s.Key)))
-        {
-            throw new InvalidCastException($"Cannot cast {NotificationName} - {typeof(TNotification).FullName} - Reciever notification has less fields");
-        }
-        foreach (var property in targetProperties)
-        {
-            if (sourceFields!.ContainsKey(property.Name) && property.CanWrite)
-            {
-                var value = sourceFields[property.Name];
-                property.SetValue(targetFields, Convert.ChangeType(value, property.PropertyType));
-            }
-            else if (!mediatorOptions.IgnoreNotificationDifferences)
-            {
-                throw new InvalidCastException($"Cannot cast {NotificationName} - Published notification has less fields");
-            }
-        }
-        return Handle((TNotification)targetFields!, cancellationToken);
+        var source = JsonConvert.SerializeObject(message); 
+        var target = JsonConvert.DeserializeObject<TNotification>(source)!; 
+        return Handle(target, cancellationToken);
     }
 
     public abstract Task Handle(TNotification message, CancellationToken? cancellationToken);
