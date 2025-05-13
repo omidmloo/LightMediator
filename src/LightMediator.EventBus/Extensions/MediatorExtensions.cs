afbd669b317ad4725ae8972eb8f831802d4c46a2
Mahmood Malekloo <mahmoodmalekloo@outlook.com>
2025-05-13 09:49:09 +0330

    feat: exception handling

--- src/LightMediator.EventBus/Extensions/MediatorExtensions.cs
+++ src/LightMediator.EventBus/Extensions/MediatorExtensions.cs
@@ -3,18 +3,44 @@
 public static class MediatorExtensions
 {
     /// <summary>
-    /// Publish an event to service bus
+    /// Publish an event to all registered event buses.
     /// </summary>
-    /// <param name="mediator"></param>
-    /// <param name="notification"></param>
-    /// <param name="publishToEventBus"></param>
+    /// <param name="mediator">Mediator instance</param>
+    /// <param name="notification">Notification to publish</param>
     /// <returns></returns>
     public static async Task PublishEvent(this IMediator mediator, INotification notification)
     {
-        var eventName = notification.GetType().Name;
-        var eventBuses = mediator.GetServiceProvider().GetServices<ILightMediatorEventBus>();
-        var tasks = eventBuses.Select(eb => eb.PublishAsync(notification)).ToList();
+        if (mediator == null) throw new ArgumentNullException(nameof(mediator));
+        if (notification == null) throw new ArgumentNullException(nameof(notification));
+
+        var eventBuses = mediator.GetServiceProvider().GetServices<ILightMediatorEventBus>().ToList();
+
+        var exceptions = new List<Exception>();
+        var tasks = new List<Task>();
+
+        foreach (var eventBus in eventBuses)
+        {
+            tasks.Add(SafePublish(eventBus, notification, exceptions));
+        }
+
         await Task.WhenAll(tasks);
+
+        if (exceptions.Any())
+        {
+            throw new EventBusAggregateException(notification.GetType(), exceptions);
+        }
+    }
+
+    private static async Task SafePublish(ILightMediatorEventBus eventBus, INotification notification, List<Exception> exceptions)
+    {
+        try
+        {
+            await eventBus.PublishAsync(notification);
+        }
+        catch (Exception ex)
+        {
+            exceptions.Add(new EventBusPublishException(eventBus.GetType(), notification.GetType(), ex));
+        }
     }
 
 }
--- src/LightMediator.EventBus/GlobalUsings.cs
+++ src/LightMediator.EventBus/GlobalUsings.cs
@@ -3,3 +3,7 @@ global using System.Reflection;
 
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
+
+global using LightMediator;
+global using LightMediator.Exceptions;
+global using LightMediator.EventBus.Exceptions;
